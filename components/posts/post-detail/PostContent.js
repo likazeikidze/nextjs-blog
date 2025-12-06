9d950c13cc6673b936fa3ab3b8e67e6dbd980d92
Lika Zeikidze <likazeikidze18@gmail.com>
2025-12-07 02:54:57 +0400

    Add Contact form + markdown code

--- components/posts/post-detail/PostContent.js
+++ components/posts/post-detail/PostContent.js
@@ -1,4 +1,7 @@
 import ReactMarkdown from "react-markdown";
+import Image from "next/image";
+import { Prism as SyntaxHighlighter } from "react-syntax-highlighter";
+import { atomDark } from "react-syntax-highlighter/dist/cjs/styles/prism";
 
 import PostHeader from "./PostHeader";
 import classes from "./PostContent.module.css";
@@ -9,7 +12,38 @@ const PostContent = ({ post }) => {
   return (
     <article className={classes.content}>
       <PostHeader title={post.title} image={imagePath} />
-      <ReactMarkdown>{post.content}</ReactMarkdown>
+      <ReactMarkdown
+        components={{
+          // Image renderer
+          img: ({ node, ...props }) => (
+            <Image
+              src={`/images/posts/${post.slug}/${props.src}`}
+              alt={props.alt}
+              width={600}
+              height={300}
+            />
+          ),
+
+          // Code block renderer
+          code({ node, inline, className, children, ...props }) {
+            const match = /language-(\w+)/.exec(className || "");
+            return !inline && match ? (
+              <SyntaxHighlighter
+                style={atomDark}
+                language={match[1]}
+                PreTag="div"
+                children={String(children).replace(/\n$/, "")}
+              />
+            ) : (
+              <code className={className} {...props}>
+                {children}
+              </code>
+            );
+          },
+        }}
+      >
+        {post.content}
+      </ReactMarkdown>
     </article>
   );
 };
