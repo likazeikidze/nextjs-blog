2da8f5c543b422c53cfa246f46c7eb62237235a1
Lika Zeikidze <likazeikidze18@gmail.com>
2025-12-08 22:31:23 +0400

    Connect contact API to MongoDB

--- app/api/contact/route.js
+++ app/api/contact/route.js
@@ -1,3 +1,5 @@
+import { MongoClient } from "mongodb";
+
 export async function POST(req) {
   const data = await req.json();
 
@@ -23,7 +25,31 @@ export async function POST(req) {
     message,
   };
 
-  console.log(newMessage);
+  let client;
+
+  try {
+    client = await MongoClient.connect(process.env.MONGODB_URI);
+  } catch (error) {
+    return new Response(
+      JSON.stringify({ message: "Could not connect to database." }),
+      { status: 500, headers: { "Content-Type": "application/json" } }
+    );
+  }
+
+  const db = client.db();
+
+  try {
+    const result = await db.collection("messages").insertOne(newMessage);
+    newMessage.id = result.insertedId;
+  } catch (error) {
+    client.close();
+    return new Response(
+      JSON.stringify({ message: "Storing message failed!" }),
+      { status: 500, headers: { "Content-Type": "application/json" } }
+    );
+  }
+
+  client.close();
 
   return new Response(
     JSON.stringify({
